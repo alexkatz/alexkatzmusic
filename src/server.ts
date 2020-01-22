d193aad652b48cc198eb9df6a61a4014a7d99b61
Alex Katz <alexanderikatz@gmail.com>
2020-01-21 19:36:52 -0500

    removed ssl-express-www

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import secure from 'ssl-express-www';
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(secure);
+// app.use(secure);
 app.use('/', express.static(path.join(__dirname, '../public')));
 app.get('*', (_request, response) => response.sendFile(path.resolve(`${__dirname}/../public/index.html`)))
 app.listen(port, () => console.log(`server listening on ${port}.`));
