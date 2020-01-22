95e2c308aa6f9fbffdbf8865e1bf8f18715aac85
Alex Katz <alexanderikatz@gmail.com>
2020-01-21 19:24:47 -0500

    ssl, proper routing in express

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import path from 'path';
+import secure from 'ssl-express-www';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(secure);
 app.use('/', express.static(path.join(__dirname, '../public')));
+app.get('*', (_request, response) => response.sendFile(path.resolve(`${__dirname}/../public/index.html`)))
 app.listen(port, () => console.log(`server listening on ${port}.`));
